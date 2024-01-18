7536fb8f2701ae0e213233b8aa89ea83c9dc99a3
Jorge Zaldívar <jzaldivar@google.com>
2024-01-18 16:35:35 +0100

    Move MatchingEngine from community to google-vertexai. Rename to VertexAIVectorStore

--- libs/partners/google-vertexai/langchain_google_vertexai/__init__.py
+++ libs/partners/google-vertexai/langchain_google_vertexai/__init__.py
@@ -1,5 +1,8 @@
 from langchain_google_vertexai.chat_models import ChatVertexAI
 from langchain_google_vertexai.embeddings import VertexAIEmbeddings
 from langchain_google_vertexai.llms import VertexAI, VertexAIModelGarden
+from langchain_google_vertexai.vectorstores import VertexAIVectorStore
 
-__all__ = ["ChatVertexAI", "VertexAIEmbeddings", "VertexAI", "VertexAIModelGarden"]
+__all__ = [
+    "ChatVertexAI", "VertexAIEmbeddings", "VertexAI", "VertexAIModelGarden", "VertexAIVectorStore"
+]
